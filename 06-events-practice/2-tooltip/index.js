f430a3fa64b76013be98e633b95846df5338aff4
Max Pekur <lider.yamal@mail.ru>
2022-04-21 15:03:36 +0500

    solved 6, 7 modules

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,58 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  element;
+
+  onPointerOver = (event) => {
+    const element = event.target.closest('[data-tooltip]');
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      document.addEventListener('pointermove', this.onPointerMove);
+    }
+  };
+
+  onPointerMove = (event) => {
+    this.moveTooltip(event);
+  };
+
+  onPointerOut = () => {
+    this.remove();
+    document.removeEventListener("pointermove", this.onPointerOver);
+  };
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  initialize() {
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    document.addEventListener("pointerover", this.onPointerOver);
+    document.addEventListener("pointerout", this.onPointerOut);
+  }
+
+  render(text) {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="tooltip">${text}</div>`;
+    this.element = div.firstElementChild;
+    document.body.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.element = null;
   }
 }
 
--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,107 @@
-import fetchJson from './utils/fetch-json.js';
+import fetchJson from "./utils/fetch-json.js";
 
-const BACKEND_URL = 'https://course-js.javascript.ru';
+const BACKEND_URL = "https://course-js.javascript.ru";
 
 export default class ColumnChart {
 
+  chartHeight = 50;
+
+  constructor({
+    label = "",
+    link = "",
+    range = {
+      from: new Date(),
+      to: new Date()
+    },
+    url = "",
+  } = {}) {
+    this.label = label;
+    this.link = link;
+    this.range = range;
+    this.url = new URL(url, BACKEND_URL);
+  
+    this.render();
+    this.update(this.range.from, this.range.to);
+  }
+
+  render() {
+
+    const bodyData = this.data != undefined ? this.getBody() : '';
+
+    if (this.element === undefined) {
+      const element = document.createElement("div");
+
+      element.innerHTML = `
+          <div class="column-chart column-chart_loading" style="--chart-height:">
+            <div class="column-chart__title">
+              Total ${this.label}
+              ${this.getLink()}
+            </div>
+            <div class="column-chart__container">
+              <div data-element="header" class="column-chart__header">100</div>
+              <div data-element="body" class="column-chart__chart">${bodyData}</div>
+            </div>
+          </div>
+        `;
+
+      this.element = element.firstElementChild;
+      return;
+    } 
+    this.element.querySelector('[data-element="body"]').innerHTML = bodyData;
+  }
+
+  getLink() {
+    return this.link
+      ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+      : "";
+  }
+
+  async getData(from, to) {
+    this.url.searchParams.set("from", from.toISOString());
+    this.url.searchParams.set("to", to.toISOString());
+    
+    return await fetchJson(this.url);
+  }
+
+  getBody() {
+    const maxValue = Math.max(...this.data);
+    const heightIndex = this.chartHeight / maxValue;
+
+    return this.data.map((item) => {
+      const percent = ((item / maxValue) * 100).toFixed(0);
+
+      return `<div style="--value: ${Math.floor(item * heightIndex)}" data-tooltip="${percent}%"></div>`;
+    })
+      .join("");
+  }
+
+  initEventListeners() {
+    
+  }
+
+  async update(from, to) {
+    this.element.classList.add('column-chart_loading');
+    const data = await this.getData(from, to); 
+
+    if (data && Object.values(data).length) {
+      this.element.classList.remove("column-chart_loading");
+    }
+    
+    this.data = Object.entries(data).map(([key, value]) => value);
+
+    this.render();
+    
+    return this.data;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    
+  }
+
+  
 }
